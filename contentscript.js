e8771a684051e5a47f79266e9c0ceec21f1e0390
Florian <florian@backbase.com>
2022-03-02 16:30:40 +0100

    WIP Initial Version

--- contentscript.js
+++ contentscript.js
@@ -4,18 +4,19 @@ function nodes(selector) {
     return Array.from(document.querySelectorAll(selector));
 }
 
-function updateNodes(settingName, selector, fn) {
+async function updateNodes(settingName, selector, fn) {
     ifEnabled(settingName, () =>
         nodes(selector)
             .filter(e => !nodeUpdated.includes(e))
-            .forEach((element, index, array) => {
-                fn(element, index, array)
+            .forEach(async (element, index, array) => {
+                await fn(element, index, array)
                 nodeUpdated.push(element);
             })
     )
 }
 
 const cache = new Map();
+
 function ifEnabled(settingName, fn) {
     if (cache.has(settingName)) {
         if (!cache.get(settingName)) {
@@ -43,10 +44,50 @@ function createHtmlButton(innerHtml, classList, onClick) {
     return button;
 }
 
+const delay = milliseconds => new Promise(resolve => {
+    setTimeout(resolve, milliseconds);
+});
 
-setInterval(() => {
-    updateNodes('downloadBtn', '.', (e) => {
+const download = async (url, name, i) => {
+    console.log("ddd");
+    setTimeout(() => {
+        const a = document.createElement('a');
+
+        a.download = name;
+        a.href = url;
+        a.style.display = 'none';
+        document.body.append(a);
+        a.click();
+
+        console.log("Download");
+
+    }, i * 1000);
+};
 
 
+const turndownService = new TurndownService();
+
+setInterval(async () => {
+    await updateNodes('downloadBtn', '#layout-section', async (e) => {
+        e.prepend(createHtmlButton('💾 Download', [], () => {
+            let markdown = ["# " + turndownService.turndown(document.querySelector('h2'))];
+            debugger
+            let downloadI = 0;
+            document.querySelectorAll('.ck-content > *').forEach((e, i) => {
+                let markdownLine = turndownService.turndown(e)
+                const lexer = marked.lexer(markdownLine);
+                if (lexer.length && (lexer[0].tokens || []).length === 1) {
+                    if (lexer[0].tokens[0].type === "image") {
+                        const image = lexer[0].tokens[0];
+                        const url = image.href;
+                        const filename = url.split('/').pop();
+                        markdownLine = markdownLine.replace(url, './' + filename);
+                        download(url, filename, downloadI++)
+                    }
+                }
+                markdown.push(markdownLine);
+            })
+            download('data:text/plain;charset=utf-8,' + encodeURIComponent(markdown.join('\n')), "README.md")
+        }))
     })
 }, 500);
